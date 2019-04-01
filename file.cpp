af567a03eb097a2315c9ecb4b6e1a18aea7adcc2
BantuD <500069424@stu.upes.ac.in>
2019-04-01 15:07:11 +0530

    Added sub func:

--- file.cpp
+++ file.cpp
@@ -11,10 +11,23 @@ class calcus{
 			cout<<"Enter two numbers to sum: ";
 			cin>>i>>k;
 			cout<<"\nThe sum of Numbers Is: "<<i+k;
-		}
+
+	}
+
+
+		  void sub(){
+
+                        cout<<"Enter two numbers to sub: ";
+                        cin>>i>>k;
+                        cout<<"\nThe sub of Numbers Is: "<<i-k;
+                      }
 };
 int main(){
 	calcus c;
 	c.sum();
+	c.sub();
 	return 0;
 }
+
+
+
